ebba492e9bfbd957944877efe49cdfc2eab9fd45
DEBASHISH BORDOLOI <webshack2018@icloud.com>
2022-11-09 00:18:13 +0530

    Create demo10000000000000000.cpp

---
+++ demo10000000000000000.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  /* int a = 100;
+  int b = 200; */
+  int a = 100, b= 200;
+  float pi =3.14;
+  char d = 'f';
+    cout << "This Was One of the Noob Journey to my Programming Language\n" << "Try to display int b = "<<b;
+    cout<<"\nThe value of Pi is : " <<pi;
+    cout<<"\nTry to disply the value of a :" << a;
+    cout<<"\nTry to display the Char value : "<<d;
+    return 0;
+       }
