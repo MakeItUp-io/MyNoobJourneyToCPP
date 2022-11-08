67ab3fe79bdc597e15236123acd31b6326890e9c
DEBASHISH BORDOLOI <webshack2018@icloud.com>
2022-11-08 23:07:42 +0530

    Create day01.cpp

---
+++ day01.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //iostream stands for standard input-output stream. This header file contains definitions of objects like cin, cout, cerr, etc.
+//using namespace std; either std::
+int main() { //main() is the function entry point
+   std::cout << "Hello Debashish Bordoloi!"; //  "" is called as string
+   return 0; // termination of program
+            }
+
